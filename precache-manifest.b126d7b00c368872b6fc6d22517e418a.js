859e126adfc5f601e100059de36227f635c276c2
joekmilne@gmail.com <jmilne@Js-MacBook-Pro.local>
2020-12-10 20:19:03 +0000

    setup react app

---
+++ precache-manifest.b126d7b00c368872b6fc6d22517e418a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b24bc20c71cc935a97a43bf1959f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c155fe7b3d0365185962",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42eb595c7b03279a9aae",
+    "url": "/static/js/2.459d66dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.459d66dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c155fe7b3d0365185962",
+    "url": "/static/js/main.5db423c0.chunk.js"
+  },
+  {
+    "revision": "02c200db6240a027d93f",
+    "url": "/static/js/runtime-main.de0f5698.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
